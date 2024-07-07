de9406b49cbca3e4ec8bc864830a325158acb77c
BiggieLion <leonrhj@gmail.com>
2024-07-06 20:18:47 -0600

    feat(project)!: modify project as monorepo and create database and config modules into common library

---
+++ libs/common/src/configuration/configuration.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validationSchema } from './validation';
+import { configuration } from './configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+      load: [configuration],
+      validationSchema,
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigurationModule {}
